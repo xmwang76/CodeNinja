c21752a33046b2823be5814a0f14cb139f43308f
xmwang76 <xmwang76@gmail.com>
2015-03-28 08:49:34 -0700

    Create WildcardMatchingII.java

---
+++ WildcardMatchingII.java
@@ -0,0 +1,16 @@
+/*
+http://www.geeksforgeeks.org/forums/topic/facebook-interview-question-for-software-engineerdeveloper-0-2-years-about-algorithms-data-str/
+
+You have to write a function checkRegex() which takes two strings as input, one string represents a regex expression and other is an input string to match with the regex expression passed as the other parameter. Return true if it matches, else false.
+
+
+Regex may contain characters ‘a-z’, ‘.’ and ‘*’ where ‘.’ matches any character and ‘*’ means 0 or more occurrences of the previous character preceding it.
+
+Examples:
+
+1) a*b matches b,ab,aab
+
+2) a.b matches aab, abb, acb,…, azb
+
+3) .* matches all the valid strings formed by using the lowercase letters
+*/
