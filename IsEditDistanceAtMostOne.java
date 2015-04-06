82b563607dc65f060eb01b0a0062091213adeb94
xmwang76 <xmwang76@gmail.com>
2015-04-05 21:13:54 -0700

    Create IsEditDistanceAtMostOne.java

---
+++ IsEditDistanceAtMostOne.java
@@ -0,0 +1,17 @@
+/*
+class IntFileIterator {
+  boolean hasNext();
+  int next();
+}
+
+class{
+  public boolean isDistanceZeroOrOne(IntFileIterator a, IntFileIterator b)；
+
+}
+// return if the distance between a and b is at most 1.. 
+// Distance: minimum number of modifications to make a=b
+// Modification:
+//   1. change an int in a
+//   2. insert an int to a
+//   3. remove an int from a
+*/
